4625aaac5319eeeb58ed08bd4be2e6f66d2ba6db
NaolDaba <nahafile@gmail.com>
2023-10-17 20:06:15 +0300

    added mapper functions for the comments

---
+++ api/Mappers/CommentMappers.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comments commentModel)
+        {
+            return new CommentDto
+            {
+                Id=commentModel.Id,
+                Title = commentModel.Title,
+                Content=commentModel.Content,
+                CreatedOn=commentModel.CreatedOn,
+                CreatedBy=commentModel.AppUser.UserName,
+                StockId=commentModel.StockId
+            };
+        }
+
+        public static Comments ToCommentFromCreate(this CreateCommentDto commentDto, int stokId)
+        {
+            return new Comments
+            {
+                Title = commentDto.Title,
+                Content=commentDto.Content,
+                StockId=stokId
+            };
+        }
+
+        public static Comments ToCommentFromUpdate(this UpdateCommentRequestDto commentDto)
+        {
+            return new Comments
+            {
+                Title = commentDto.Title,
+                Content=commentDto.Content,
+            };
+        }
+
+
+    }
+}
