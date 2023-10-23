7b13cce5dfa980f39fcaaf22233503cd59550ecc
NaolDaba <nahafile@gmail.com>
2023-10-23 20:10:15 +0300

    created the contrat functions for the comments to be implemented

---
+++ api/Interfaces/ICommentRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task <List<Comments>> GetAllAsync();
+        Task <Comments?> GetByIdAsync(int id);
+        Task <Comments> CreateAsync(Comments commentModel);
+        Task <Comments?> UpdateAsync(int id, Comments commentModel);
+        Task<Comments?> DeleteAsync(int id);
+    }
+}
